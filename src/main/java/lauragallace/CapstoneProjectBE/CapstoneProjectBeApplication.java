41f638a493b4e08838982a2072b7b98bb65ada88
Lauragallace <143189799+Lauragallace@users.noreply.github.com>
2024-02-11 14:54:32 +0100

    first commit

---
+++ src/main/java/lauragallace/CapstoneProjectBE/CapstoneProjectBeApplication.java
@@ -0,0 +1,13 @@
+package lauragallace.CapstoneProjectBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lauragallace/CapstoneProjectBE/CapstoneProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package lauragallace.CapstoneProjectBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
